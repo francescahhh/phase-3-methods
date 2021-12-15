ea0207573ceeb3284f72a0ded0e7f74c064e1f53
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-12-14 18:19:30 -0700

    commit tree 7f0b10bf8ce9548a30839d72002cc3e98cdce6ef

--- methods.rb
+++ methods.rb
@@ -1,1 +1,41 @@
-# Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+greet_programmer 
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+  
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end 
+
+greet_with_default "Sunny"
+
+
+def add num1, num2
+    p num1 + num2 
+end
+
+add(1, 2)
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end 
+
+halve(6)
+
